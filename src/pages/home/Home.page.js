8fc625036a22db6478221063445717ab5ce9f3bf
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-08-18 00:42:07 +0630

    update home page

--- src/pages/home/Home.page.js
+++ src/pages/home/Home.page.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { ScrollIndicator } from '../../components/animations/anime.component'
+import Footer from '../../components/footer/footer.component'
 import Navigation from '../../components/nav/nav.component'
-import Canvas from './canvas.component'
+import Body from './body.component'
 
 function Home() {
     return (
         <>
             <Navigation/>
-            <Canvas/>
+            <ScrollIndicator/>
+            <Body/>
+            <Footer/>
         </>
     )
 }

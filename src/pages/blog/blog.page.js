2b5ec7cb1dda12a9eb6e06d00a4ba525fbe316a7
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-08-22 01:59:17 +0630

    init main blog page

---
+++ src/pages/blog/blog.page.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+import Navigation from "../../components/nav/nav.component";
+
+const Blog = () => {
+    
+
+    return (
+        <>
+            <Navigation/>
+        </>
+    )
+}
+
+export default Blog

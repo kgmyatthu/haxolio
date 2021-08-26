1867ffb4731e0be4f95033521f50acd702b12fd5
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-08-27 02:31:15 +0630

    update blog page

--- src/pages/blog/blog.page.js
+++ src/pages/blog/blog.page.js
@@ -1,15 +1,61 @@
 import React, { useEffect, useState } from 'react'
-
+import axios from 'axios';
 import Navigation from "../../components/nav/nav.component";
+import {ArticleLists, ArticleSearchResult} from "./body.component.js";
+import {ArticleDetail, IntroFixedCol} from '../../components/card/card.component';
+import styles from './blog.module.css';
+import { useParams } from 'react-router';
+import { ScrollIndicator } from '../../components/animations/anime.component';
+import Footer from '../../components/footer/footer.component';
+
 
 const Blog = () => {
     
-
     return (
         <>
-            <Navigation/>
+            <div className={`${styles.bgblack}`}>
+                <Navigation/>
+                <ScrollIndicator/>
+                <ArticleLists/>
+                <Footer/>
+            </div>
+        </>
+    )
+}
+export default Blog;
+
+
+
+export const Article = () => {
+    const { slug } = useParams();
+    useEffect(()=>{ 
+        console.log(slug);
+    },[])
+    return (
+        <>
+            <div className={`${styles.bgblack}`}>
+                <Navigation/>
+                <ScrollIndicator/>
+                <IntroFixedCol>
+                    <ArticleDetail slug={slug}/>
+                </IntroFixedCol>
+                <Footer/>
+            </div>
         </>
     )
 }
 
-export default Blog
+export const BlogResult = () =>{
+    const {keyword} = useParams();
+    return (
+        <>
+            <div className={`${styles.bgblack}`}>
+                <Navigation/>
+                <ScrollIndicator/>
+                <ArticleSearchResult keyword={keyword}/>
+                <Footer/>
+            </div>
+        </>
+    )
+}
+

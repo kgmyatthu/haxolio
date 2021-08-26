e5e9a12c1b7594c83e93d88de47d5a85093c3e0e
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-08-27 02:29:10 +0630

    add error boundary and another handling components

---
+++ src/components/handler/handler.component.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './handler.module.css';
+
+const Notfound = () => {
+    return (
+        <div className={styles.notfound}>
+            <div className={styles.notfound_child}>
+                <h1><small>0x</small>404</h1>
+                <p>The page you're looking for does not exist.</p>
+                <Link to="/">Go Back</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Notfound
+
+
+export const DataNotFound = () =>{
+    return (
+        <div className={styles.data_not_found}>
+            <div className={styles.data_not_found_child}>
+                <h1><small>0x</small>404</h1>
+                <p>Data Doesnt seem to be exist</p>
+            </div>
+        </div>
+    )
+}
+
+
+export const GeneralError = () => {
+    return (
+        <div className={styles.notfound}>
+            <div className={styles.notfound_child}>
+                <h1><small>0x</small>500</h1>
+                <p>Something's wrong with the site if this keep happening, please contact the site owner</p>
+                <Link to="/">Go Back</Link>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+export class ErrorBoundary extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { hasError: false };
+    }
+  
+    static getDerivedStateFromError(error) {
+      // Update state so the next render will show the fallback UI.
+      return { hasError: true };
+    }
+  
+    componentDidCatch(error, errorInfo) {
+      // You can also log the error to an error reporting service
+      console.log(error, errorInfo);
+    }
+  
+    render() {
+      if (this.state.hasError) {
+        // You can render any custom fallback UI
+        return <GeneralError/>
+      }
+  
+      return this.props.children; 
+    }
+  }

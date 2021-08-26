bc63f6eb4311aec51862da7694aeb79fdffae055
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-08-27 02:27:17 +0630

    add new route and error boundary

--- src/App.js
+++ src/App.js
@@ -4,31 +4,36 @@ import { BrowserRouter as Router, HashRouter, Switch, Route } from "react-router
 import Navigation from './components/nav/nav.component';
 
 import Home from './pages/home/Home.page';
-import Notfound from './components/handler/notfound.component';
+import Notfound, { ErrorBoundary, GeneralError } from './components/handler/notfound.component';
 import Loading from './components/animations/loading.component';
-import Blog, { Article } from './pages/blog/blog.page';
+import Blog, { Article, BlogResult } from './pages/blog/blog.page';
 
 function App() {
   return (
     <>
-    <Router>
-      <HashRouter>
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route exact path="/blog">
-            <Blog/>
-          </Route>
-          <Route exact path="/blog/:slug">
-            <Article/>
-          </Route>
-          <Route path="*">
-            <Notfound/>
-          </Route>
-        </Switch>
-      </HashRouter>
-    </Router>
+      <Router>
+        <ErrorBoundary>
+          <HashRouter>
+            <Switch>
+              <Route exact path="/">
+                <Home/>
+              </Route>
+              <Route exact path="/blog">
+                <Blog/>
+              </Route>
+              <Route exact path="/blog/:slug">
+                <Article/>
+              </Route>
+              <Route exact path="/blog/search/:keyword">
+                <BlogResult/>
+              </Route>
+              <Route path="*">
+                <Notfound/>
+              </Route>
+            </Switch>
+          </HashRouter>
+        </ErrorBoundary>
+      </Router>
     </>
   );
 }

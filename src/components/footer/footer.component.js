21df6cde22b2690ea0058ef27c8b4bf84e717771
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-09-13 00:09:11 +0630

    update contacts

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -16,7 +16,7 @@ const Footer = () => {
                         by 
                         <span className="text-lead" style={{color:"white"}}>&nbsp;Kaung Myat Thu&nbsp;</span>
                         <br/>
-                        <span className={styles.icons} style={{margin:"0.5rem"}}><a href="https://github.com/kmt29" target="_blank" rel="noreferrer"><FaGithub size={20}/></a></span>
+                        <span className={styles.icons} style={{margin:"0.5rem"}}><a href="https://github.com/kgmyatthu" target="_blank" rel="noreferrer"><FaGithub size={20}/></a></span>
                         <span className={styles.icons} style={{margin:"0.5rem"}}><a href="https://www.facebook.com/kaungmyatthu29/" target="_blank" rel="noreferrer"><FaFacebook size={20}/></a></span>
                         <span className={styles.icons} style={{margin:"0.5rem"}}><a href="https://twitter.com/mgkaungmyatthu" target="_blank" rel="noreferrer"><FaTwitter size={20}/></a></span>
                         <span className={styles.icons} style={{margin:"0.5rem"}}><a href="https://www.linkedin.com/in/kaung-myat-thu-3925531b6/" target="_blank" rel="noreferrer"><FaLinkedin size={20}/></a></span>
